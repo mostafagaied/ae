c2a425a872ed6971708fe46db249a9c9d3ca4d6f
mostafagaied <mostafagayed018@gmail.com>
2024-12-18 19:24:12 +0100

    Add files via upload

---
+++ web2/questions.js
@@ -0,0 +1,139 @@
+let questions = [
+    {
+       numb: 1,
+       question: "What does HTML stand for?",
+       answer: "C. Hyper Text Markup Language",
+       image:"p.png",
+       options: [
+        "A. Hyper Type Multi Language",
+        "B. Hyper Text Multiple Language",
+        "C. Hyper Text Markup Language",
+        "D. Home Text Multi Language",
+
+       ]     
+    },
+    {
+        numb: 2,
+        question: "Which of the following tags is used to create a hyperlink in HTML?",
+        answer: "B. a",
+        image:"p.png",
+        options: [
+          "A. linj",
+          "B. a",
+          "C. href",
+          "D. p",
+        ]     
+     },
+     {
+        numb: 3,
+        question: "What does the <br> tag do in HTML?",
+        answer:"C. Inserts a line break",
+        image:"p.png",
+        options: [
+          "A. Makes text bold",
+          "B. Inserts a horizontal line",
+          "C. Inserts a line break",
+          "D. Creates a paragraph",
+        ]     
+     },
+     {
+        numb: 4,
+        question: "Which of the following statements about the <canvas> tag is true?",
+        answer:"B. It requires JavaScript to draw graphics or animations",
+        image:"p.png",
+        options: [
+         "A.  It is used to display multimedia content like audio and video",
+         "B. It requires JavaScript to draw graphics or animations",
+         "C. It is used to define container-based sections of the webpage",
+         "D. It automatically resizes graphics to fit the container",
+        ]     
+     },
+     {
+        numb: 5,
+        question: "Which of these is NOT a valid HTML5 element??",
+        answer: "D. blink",
+        image: "p.png",
+        options: [
+         "A. section",
+         "B. article",
+         "C. footer",
+         "D. blink",
+        ]     
+     },
+     
+] 
+let questionsf = [
+   {
+      numb: 1,
+      question: "which club is this ?",
+      answerf: "A. Barcelona",
+      image:"barca.png",
+      options: [
+       "A. Barcelona",
+       "B. real madrid",
+       "C. Olampique Baja",
+       "D. PSG",
+
+      ]     
+   },
+   {
+       numb: 2,
+       question: "which movie has this lyric  ?",
+       answerf: "B. Harry Potter",
+       image:"harry2.mp3",
+       options: [
+         "A. lord of the ring",
+         "B. Harry Potter",
+         "C. Titanic",
+         "D. Spider Man",
+       ]     
+    },
+    {
+       numb: 3,
+       question: "whats the name of this anime?",
+       answerf: "C. Attack on titane ",
+       image:"attaque.mp4",
+       options: [
+         "A. Naruto",
+         "B. One piece",
+         "C. Attack on titane ",
+         "D. Death Note",
+       ]     
+    },
+    {
+       numb: 4,
+       question: "wahts the name of the artist?",
+       answerf: "D. Beethoven",
+       image:"beethoven4.mp3",
+       options: [
+        "A. Johann Sebastian",
+        "B. Mozart",
+        "C. Ozuna",
+        "D. Beethoven",
+       ]     
+    },
+    {
+       numb: 5,
+       question: "whish is the capitale of turkey?",
+       answerf: "B. Ankra",
+       image:"foot.png",
+       options: [
+        "A. Izmir",
+        "B. Ankra",
+        "C. Istanbul",
+        "D. Bursa",
+       ]     
+    },
+    {
+      numb: 6,
+      question: "What's the name of this goalkeeper?",
+      answerf: "A. Emiliano Martínez",
+      image:"martinaz.jpg",
+      options: [
+       "A. Emiliano Martínez",
+       "B. Neur",
+       "C. Bechir Ben Said",
+       "D. Courtois",
+      ]     
+   },
+] 
